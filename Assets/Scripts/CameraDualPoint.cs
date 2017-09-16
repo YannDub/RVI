1e588cc8f31d1e6e95f0cd84d75c76b82f60244e
Yann Dubois <yann59.dubois@gmail.com>
2017-09-16 14:38:04 +0200

    tp6-camera

---
+++ Assets/Scripts/CameraDualPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class CameraDualPoint : MonoBehaviour {
+
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (UnityEditor.Selection.gameObjects.Length >= 1 && UnityEditor.Selection.gameObjects [0] != null && UnityEditor.Selection.gameObjects [0] != this.gameObject)
+			target = UnityEditor.Selection.gameObjects [0].transform;
+
+		this.transform.LookAt (target);
+	}
+}
