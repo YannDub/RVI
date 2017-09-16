e4e532dd39e19a3da4a053d2e063a8f24088877d
Yann Dubois <yann59.dubois@gmail.com>
2017-09-16 18:15:52 +0200

    tp6-manip

---
+++ Assets/Scripts/InteractionSouris.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSouris : MonoBehaviour {
+	private GameObject text;
+	private GameObject objCollider = null;
+	private Plane plane;
+
+	public float speed = 10f;
+	public bool screen_space = false;
+
+	// Use this for initialization
+	void Start () {
+		text = new GameObject ();
+		text.AddComponent<GUIText> ();
+		this.plane = new Plane ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit) && this.objCollider == null) {
+				this.objCollider = hit.collider.gameObject;
+			} 
+
+			if (!screen_space) {
+				// x,z game object move
+				this.objCollider.gameObject.transform.Translate (new Vector3 (Input.GetAxis ("Mouse X") * Time.deltaTime * speed, 0, Input.GetAxis ("Mouse Y") * Time.deltaTime * speed), Space.World);
+				Debug.Log (Input.GetAxis ("Mouse X") * Time.deltaTime);
+			} else {
+				float distance = 0;
+				this.plane = new Plane (-Camera.main.transform.forward, this.objCollider.transform.position);
+				if (this.plane.Raycast (ray, out distance)) {
+					Vector3 projection = ray.GetPoint (distance);
+					Debug.Log (projection);
+					this.objCollider.transform.position = projection;
+				}
+			}
+		} else {
+			this.objCollider = null;
+		}
+
+		if (this.objCollider != null) {
+			text.GetComponent<GUIText> ().text = this.objCollider.name + "\n" + this.objCollider.transform.position;
+			text.GetComponent<GUIText> ().pixelOffset = new Vector2 (Screen.width / 2, Screen.height / 2);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+DUBOIS Yann
+M2IVI
+RVI
+
+# TP1
+
+# TP2
+
+# TP3
+
+# TP4
+
+# TP5
+
+# TP6
+## Partie 2
+### Etape 3
+
+Question 2 :
+
+On peut faire une intéraction co-localisé lorsque les objets seul déplace
+celon x et z. Mais il faudra faire très attention à la façon de calculer la
+transformation car le plan choisit ne sera plus celui qui est face à la caméra.
+Si l'angle entre la caméra et le plan x,z est petit, il se peut qu'avec un très
+petit mouvement de la souris, l'objet sélectionné puisse avoir un déplacement
+très rapide.
