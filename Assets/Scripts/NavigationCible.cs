7ef65d067f1c9d01cc937d85f77f8b9d1b6381ce
Yann Dubois <yann59.dubois@gmail.com>
2017-09-23 18:27:26 +0200

    Better rotation for tp6_navig

--- Assets/Scripts/NavigationCible.cs
+++ Assets/Scripts/NavigationCible.cs
@@ -8,6 +8,7 @@ public class NavigationCible : MonoBehaviour {
 	private Vector3 targetScreen;
 	private Vector3 currentPositionMouse;
 	private Vector3 initCamPos;
+	private Ray targetRay;
 
 	// Use this for initialization
 	void Start () {
@@ -20,22 +21,27 @@ public class NavigationCible : MonoBehaviour {
 
 		if (Input.GetMouseButtonDown (0)) {
 			this.targetScreen = Input.mousePosition;
-			Ray ray = Camera.main.ScreenPointToRay (this.targetScreen);
+			this.targetRay = Camera.main.ScreenPointToRay (this.targetScreen);
 			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit)) {
-				Vector3 hitPoint = ray.GetPoint (hit.distance - 1);
+			if (Physics.Raycast (this.targetRay, out hit)) {
+				Vector3 hitPoint = this.targetRay.GetPoint (hit.distance - 1);
 
 				this.initCamPos = this.transform.position;
 				target = new Vector3 (hitPoint.x, this.transform.position.y, hitPoint.z);
 			}
 		}
 
+		float angle = Input.GetAxis ("Mouse X");
+
 		if (Input.GetMouseButton (0)) {
 			float t = Mathf.Clamp (currentPositionMouse.y / targetScreen.y, 0f, 1f);
 			Debug.Log (currentPositionMouse.y / targetScreen.y);
 			this.transform.position = target * (1f - t) + this.initCamPos * t;
+			Ray ray = Camera.main.ScreenPointToRay (currentPositionMouse);
+			angle = Mathf.Atan2 (Vector3.Dot (Vector3.Cross (this.targetRay.direction, ray.direction), this.transform.up), Vector3.Dot (this.targetRay.direction, ray.direction)) * Mathf.Rad2Deg;
+			angle = this.transform.rotation.y - angle;
 		}
 
-		this.transform.Rotate (Input.GetAxis ("Mouse X") * new Vector3 (0, 1, 0));
+		this.transform.Rotate (angle * new Vector3 (0, 1, 0));
 	}
 }
